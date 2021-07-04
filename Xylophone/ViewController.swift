dbdb178a7f6de0c941891dd6ed6f5ac917025b9c
romegab <idstoilov@gmail.com>
2021-07-04 02:23:28 -0700

    added main functionalty
    added sounds play and touchung interaction

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    let delay = 0.2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay){
+            
+            sender.alpha = 1
+        }
+        
+    }
     
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
 
+            player = try! AVAudioPlayer(contentsOf: url!)
+        
+        player?.play()
+    }
 }
 
